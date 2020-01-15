e43c23417a8a860958041be51f9145f5c45d2b62
ivan <aliakseipavlenkoed@gmail.com>
2020-01-15 15:40:57 +0300

    Refactor App js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
+
 import Header from './components/organisms/Header'
 import Footer from './components/organisms/Footer'
 import Main from './components/organisms/Main'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-    
-  );
-}
 
-export default App;
+export default () => 
+<>
+  <Header />
+  <Main />
+  <Footer />
+</>;
