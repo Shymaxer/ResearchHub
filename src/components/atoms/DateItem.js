53db9d1c3487011a36fac3c373840c7e58cca151
ivan <aliakseipavlenkoed@gmail.com>
2020-01-15 15:41:29 +0300

    Refactor DateItem

--- src/components/atoms/DateItem.js
+++ src/components/atoms/DateItem.js
@@ -1,18 +1,42 @@
-import React from "react"
+import React, {Component} from "react";
 
 
 
-function DateItem(props) {
-    return (
-        <div className="date-item">
-            <h4 className="title title--sm3">{props.title}<p className="star">{props.star}</p></h4>
-            <input className="input" maxLength={props.maxLength}
-             id={props.id}
-              placeholder={props.placeholder}
-              onKeyUp = {e => {e.target.value = e.target.value.replace(/[^\d]/g,'')}}
-              defaultValue={props.value}></input>
-        </div>       
-    )
+class DateItem extends Component {
+    constructor({value}) {
+        super();
+
+        this.state = {
+            inputValue: value,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange({target}) {
+        const inputValue = target.value.replace(/[^\d]/g,'');
+
+        this.setState({
+            inputValue, 
+        })
+    }
+
+    render() {
+        const {title,star, id, maxLength, placeholder, value} = this.props;
+        const {inputValue} = this.state;
+
+        return (
+            <div className="date-item">
+                <h4 className="title title--sm3">{title}<p className="star">{star}</p></h4>
+                <input className="input" maxLength={maxLength}
+                    id={id}
+                  placeholder={placeholder}
+                  value={inputValue}
+                  onChange={this.handleChange}
+                  ></input>
+            </div>       
+        )
+    }
 }
 
 export default DateItem
